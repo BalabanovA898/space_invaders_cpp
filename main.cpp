bc57f35c7aa38b3a45582136810ffa35692422dd
Balabanov Andrey <balabaniada06@mail.ru>
2024-07-06 20:04:57 +0700

    Implement an object structure

--- main.cpp
+++ main.cpp
@@ -3,31 +3,32 @@
 
 typedef unsigned short us;
 
+struct Sprites {
+    SDL_Surface *sprites[512];
+    int last;
+};
+
+
+void ccr (int res, const char *e);
 int init (SDL_Surface **sur, SDL_Window **win, us w, us h); 
-int load_image (SDL_Surface **img, const char *path);
+int load_image (Sprites &sprites, const char *path);
 void quit (SDL_Window **win, SDL_Surface **sur);
 
 int main (void) {
     const us HEIGHT = 255;
     const us WIDTH = 255;
 
-    SDL_Surface* screen_surface = nullptr;
-    SDL_Window* window = nullptr;      
-    SDL_Surface* cat = nullptr;
+    SDL_Surface *screen_surface = nullptr;
+    SDL_Window *window = nullptr;
 
-    if (init(&screen_surface, &window, WIDTH, HEIGHT) != 0) {
-        std::cerr << "Error while initializing." << std::endl;
-        return -1;
-    }
-    if (load_image(&cat, "./assets/cat.bmp") != 0) {
-        std::cerr << "Error while loading a file." << std::endl;
-        return -1;
-    }
-    
-    SDL_Surface *sprites[512] = {cat};
+    Sprites objects;
+    objects.last = 0;
 
-    SDL_BlitSurface(cat, nullptr, screen_surface, nullptr);
-    
+    ccr(init(&screen_surface, &window, WIDTH, HEIGHT), "Error while initializing.");
+    ccr(load_image(objects, "./assets/cat.bmp"), "Error with loading a file!");
+
+    SDL_BlitSurface(objects.sprites[0], nullptr, screen_surface, nullptr);
+
     SDL_UpdateWindowSurface(window);    
     SDL_Delay(2000);
     
@@ -35,8 +36,15 @@ int main (void) {
     return 0;
 }
 
+void ccr (int res, const char *e) {
+    if (res != 0) {
+        std::cerr << "Error! " << e << std::endl;
+        exit(-1);
+    }
+}
+
 int init (SDL_Surface **sur, SDL_Window **win, us w, us h) {
-    if (SDL_Init(SDL_INIT_EVERYTHING) == 1) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         std::cerr << "Error with initializing." << std::endl;
         return -1;
     }
@@ -52,12 +60,13 @@ int init (SDL_Surface **sur, SDL_Window **win, us w, us h) {
     return 0;
 }
 
-int load_image (SDL_Surface **img, const char *path) {
-    *img = SDL_LoadBMP(path);
-    if (*img == nullptr) {
+int load_image (Sprites &sprites, const char *path) {
+    SDL_Surface *img = SDL_LoadBMP(path);
+    if (img == nullptr) {
         return -1;
-    }    
-    return 0; 
+    }
+    sprites.sprites[sprites.last++] = img;
+    return 0;
 }
 
 void quit (SDL_Window **win, SDL_Surface **sur) {
